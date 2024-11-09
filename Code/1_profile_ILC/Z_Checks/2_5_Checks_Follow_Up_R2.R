2ad6665b9dbd2238c52e03024567e264da8403b2
Archi11Gupta <146094821+Archi11Gupta@users.noreply.github.com>
2024-11-09 18:44:12 +0530

    organizing R scripts and do files changing names of the datasets for consistency

--- Code/1_profile_ILC/Z_Checks/2_5_Checks_Follow_Up_R2.R
+++ Code/1_profile_ILC/Z_Checks/2_5_Checks_Follow_Up_R2.R
@@ -108,9 +108,42 @@ overleaf <- function() {
   return(overleaf)
 }
 
+
+Intermediate <- function() {
+  # Return a hardcoded path that depends on the current user, or the current 
+  # working directory for an unrecognized user. If the path isn't readable,
+  # stop.
+  
+  user <- Sys.info()["user"]
+  
+  if (user == "asthavohra") { 
+    path = "/Users/asthavohra/Library/CloudStorage/Box-Box/India Water project/2_Pilot/Data/"
+  } 
+  else if (user=="akitokamei"){
+    path = "/Users/akitokamei/Box Sync/India Water project/2_Pilot/Data/"
+  } 
+  else if (user == "Archi Gupta"){
+    path = "C:/Users/Archi Gupta/Box/Data/8_Intermediate Datasets/" 
+  } 
+  else {
+    warning("No path found for current user (", user, ")")
+    path = getwd()
+  }
+  
+  stopifnot(file.exists(path))
+  return(path)
+}
+
+# set working directory
+knitr::opts_knit$set(root.dir = Intermediate())
+
+
+
+
+
 #------------------------ Load the data ----------------------------------------#
 
-df.temp <- read_dta(paste0(user_path(),"2_deidentified/1_6_Followup_R2_cleaned.dta" ))
+df.temp <- read_dta(paste0(Intermediate(),"1_2_Followup.dta" ))
 df.preload <- read_xlsx(paste0(user_path(),"99_Preload/FollowupR2_preload_4 mar 2024.xlsx"))
 #------------------------ Apply the labels for variables  ----------------------------------------#
 
